ecb39217871b4ab1794e24d1e063cdd9b8ce3fcd
CaoYan <4221273+linuxyan@users.noreply.github.com>
2024-06-16 19:48:46 +0800

    Add wx

---
+++ wx.py
@@ -0,0 +1,15 @@
+import requests
+import os
+
+
+def get_access_token(appid, secret):
+    url = 'https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid={}&secret={}'.format(appid, secret)
+    response = requests.get(url)
+    data = response.json()
+    return data
+
+
+if __name__ == 'main':
+    appid = os.environ.get("WX_APPID")
+    secret = os.environ.get("WX_SECRET")
+    print(get_access_token(appid, secret))
