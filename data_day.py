640dcc66ed97d006754abde38b8bdfd250296c40
CaoYan <4221273+linuxyan@users.noreply.github.com>
2025-04-29 19:29:54 +0800

    Update xq_a_token

--- data_day.py
+++ data_day.py
@@ -50,7 +50,7 @@ def day_data_update() -> pd.DataFrame:
 def get_stock_day_data(symbol):
     try:
         # 获取股票日数据指标数据
-        indicator_data = ak.stock_individual_spot_xq(symbol=add_prefix(symbol),token='6c836dc4811164266e8f0b5f6b5f1db5839a48e1')
+        indicator_data = ak.stock_individual_spot_xq(symbol=add_prefix(symbol),token='84ee047de53c45257412fe452286f0b0fce68b9b')
 
         items = {
             '基金份额/总股本': 'equity_cap',
